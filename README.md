5f278bbe7f4b7cdef24ac8fbc63d4c4249339b9b
Anand Kumar <125195651+Anandraj3551@users.noreply.github.com>
2023-06-10 12:51:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# TripPlanner.github.io
---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "tour";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if (!$conn) { // If Check Connection
+    die("<script>alert('Connection Failed.')</script>");
+}
+?>
